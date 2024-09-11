1a85ee1bf0d17471e49b7528910e849c65d642ed
lokesh <152796435+lokeshloki1122@users.noreply.github.com>
2024-09-11 14:06:32 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull lokeshloki1122/demo-python-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 lokeshloki1122/demo-python-app
