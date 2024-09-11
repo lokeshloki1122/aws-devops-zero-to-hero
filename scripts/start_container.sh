2b45a1e286c4c59bc15c10e5b1acf1d6444c3c2c
lokesh <152796435+lokeshloki1122@users.noreply.github.com>
2024-09-11 15:14:07 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull lokeshloki1122/demo-python-app:latest
+docker pull lokeshloki1122/demo-python-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 lokeshloki1122/demo-python-app
